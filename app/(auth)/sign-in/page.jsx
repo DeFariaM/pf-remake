cc34f0c1017acd3e8e61e2ac03aba591d11c1f5f
MDeFaria <maria.defariasf@gmail.com>
2024-10-01 22:40:40 -0400

    [added] control of inputs

--- app/(auth)/sign-in/page.jsx
+++ app/(auth)/sign-in/page.jsx
@@ -1,21 +1,57 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 import Btn from "@/components/Btn";
 import InputField from "@/components/InputField";
-import Link from "next/link";
+import { signIn } from "@/firebase/auth";
+import { handleInputs } from "@/helpers/handleInputs";
 
 export default function SignIn() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
     <div>
       <h1 className="text-center font-bebas text-2xl text-bgray">Ingresar</h1>
       <form className="input-cont">
-        <InputField label={"Email"} name={"email"} type="email" />
-        <InputField label={"Contraseña"} name={"password"} type="password" />
-        <Btn text={"Registrarse"} />
+        <InputField
+          label={"Email"}
+          name={"email"}
+          type="email"
+          value={user.email}
+          onChange={(e) => handleInputs(e, setUser)}
+        />
+
+        <InputField
+          label={"Contraseña"}
+          name={"password"}
+          type="password"
+          value={user.password}
+          onChange={(e) => handleInputs(e, setUser)}
+        />
+        <Btn
+          text={"Ingresar"}
+          onClick={async (e) => {
+            e.preventDefault();
+            await signIn(user);
+          }}
+        />
       </form>
       <div className="mt-4">
         <p className="text-center text-sm text-bgray">
+          <Link
+            className="underline transition-colors duration-500 hover:text-brand"
+            href={"/sign-up"}
+          >
+            ¿Olvidaste tu contraseña?{" "}
+          </Link>
+        </p>
+        <p className="mt-4 text-center text-sm text-bgray">
           ¿Aún no tienes una cuenta?{" "}
           <Link
-            className="text-bgray underline transition-colors duration-500 hover:text-brand"
+            className="underline transition-colors duration-500 hover:text-brand"
             href={"/sign-up"}
           >
             Registrarse
--- app/(auth)/sign-up/page.jsx
+++ app/(auth)/sign-up/page.jsx
@@ -1,19 +1,51 @@
 "use client";
+import Link from "next/link";
+import { useState } from "react";
 import Btn from "@/components/Btn";
 import InputField from "@/components/InputField";
-import Link from "next/link";
+import { signUp } from "@/firebase/auth";
+import { handleInputs } from "@/helpers/handleInputs";
 
 export default function SignUp() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
+
   return (
     <div>
       <h1 className="text-center font-bebas text-2xl text-bgray">
         Registrarse
       </h1>
       <form className="input-cont">
-        <InputField label={"Nombre"} name={"name"} />
-        <InputField label={"Email"} name={"email"} type="email" />
-        <InputField label={"Contraseña"} name={"password"} type="password" />
-        <Btn text={"Registrarse"} />
+        <InputField
+          label={"Nombre"}
+          name={"name"}
+          value={user.name}
+          onChange={(e) => handleInputs(e, setUser)}
+        />
+        <InputField
+          label={"Email"}
+          name={"email"}
+          type="email"
+          value={user.email}
+          onChange={(e) => handleInputs(e, setUser)}
+        />
+        <InputField
+          label={"Contraseña"}
+          name={"password"}
+          type="password"
+          value={user.password}
+          onChange={(e) => handleInputs(e, setUser)}
+        />
+        <Btn
+          text={"Registrarse"}
+          onClick={async (e) => {
+            e.preventDefault();
+            await signUp(user);
+          }}
+        />
       </form>
       <div className="mt-4">
         <p className="text-center text-sm text-bgray">
