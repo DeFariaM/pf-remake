e33b15823c05dbb075e1dc942779481a1d526fa9
MDeFaria <maria.defariasf@gmail.com>
2024-10-01 21:25:43 -0400

    [fixed] analytics error environment

--- firebase/config.js
+++ firebase/config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
@@ -21,7 +21,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+export const analytics =
+  typeof window !== "undefined" && isSupported() ? getAnalytics() : null;
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
