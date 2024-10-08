d91726c29cea5a519311c9509de02734fe48d6f3
MDeFaria <maria.defariasf@gmail.com>
2024-10-08 13:43:36 -0400

    [change] main page of the app

---
+++ app/(app)/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi error quis
+      delectus consequatur aperiam excepturi, maiores accusantium, quos placeat
+      tempore eveniet et eius vel labore dignissimos, quae ut. Ducimus,
+      deleniti!
+    </div>
+  );
+}
